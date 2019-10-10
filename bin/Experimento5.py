ddae00864bc3d1603e637469df67bfbfdbc03548
delgado tomas <tdelgado@dc.uba.ar>
2019-10-10 17:57:45 -0300

    Experimento5ConResultados

--- bin/Experimento5.py
+++ bin/Experimento5.py
@@ -1,7 +1,9 @@
+"""Clasifica el conjunto de entrada usando el mejor clasificador encontrado
 
-#Nota: Asumimos que el .so generado (sentiment.cpython....so) está en la carpeta
-#`notebooks/`
+Nota: Asumimos que el .so generado (sentiment.cpython....so) está en la carpeta
+`notebooks/`
 
+"""
 # Estas dos líneas permiten que python encuentre la librería sentiment en notebooks/
 import sys
 sys.path.append("../notebooks/")
@@ -62,72 +64,50 @@ def get_instances(df, SIZE_TRAIN, SIZE_TEST):
     print("Cantidad de instancias de test = {}".format(len(text_test)))
     return text_train, label_train, text_test, label_test
 
-def run_test(df,TRAIN_SIZE = 6225,TEST_SIZE = 500,ALPHA = None,K = None,BINARIO = False, NEGACIONES = False, NORMA_PESADA = False, IDF = False, STOP_WORDS = False):
+if __name__== '__main__':
+
+    BINARIO = True
+    IDF = True
+    NEGACIONES = True
+    TEST_SIZE = 1000
+    STOP_WORDS = True
+
     print("--------------------------------------------------------")
     print("Test empezado con:")
-    print("Train size:",TRAIN_SIZE)
     print("Test size:",TEST_SIZE)
-    print("Alpha:",ALPHA)
-    print("K:",K)
     print("Negaciones:",NEGACIONES)
     print("Binario:",BINARIO)
-    print("Norma pesada:",NORMA_PESADA)
     print("Stop words:",STOP_WORDS)
     print("IDF:",IDF)
-    text_train, label_train, text_test, label_test = get_instances(df,TRAIN_SIZE,TEST_SIZE)
 
-    print("Vectorizando...")
-    X_train, y_train, X_test, y_test = vectorizar(text_train, label_train, text_test, label_test, BINARIO, IDF, NEGACIONES, STOP_WORDS)
-    if ALPHA != None:
-        print("Obteniendo componentes principales...")
-        pca = PCA(ALPHA)
-        pca.fit(X_train.todense())
-        X_train = pca.transform(X_train, ALPHA)
-        X_test = pca.transform(X_test, ALPHA)
-
-    clf = KNNClassifier(K)
-
-    clf.fit(X_train, y_train)
-    print("Prediciendo...")
-    if not NORMA_PESADA:
-        y_pred = clf.predict(X_test)
-    else:
-        y_train_norm = y_train - y_train.mean()
-        ystd = np.std(y_train)
-
-        covarianzas = np.zeros(X_train.shape[1])
-        correlaciones = np.zeros(X_train.shape[1])
-        for i in range(X_train.shape[1]):
-            if ALPHA:
-                covarianzas[i] = ((X_train[:,i]-X_train[:,i].mean())*y_train_norm).sum()
-                correlaciones[i] = covarianzas[i]/(np.std(X_train[:,i])*ystd)
-            else:
-                covarianzas[i] = (((X_train.todense())[:,i]-X_train[:,i].mean())*y_train_norm).sum()
-                correlaciones[i] = covarianzas[i]/(np.std((X_train.todense())[:,i])*ystd)
-        y_pred = clf.predict_weighted(X_test,np.abs(correlaciones))
-    print("Test finalizado")
-    acc = accuracy_score(y_test, y_pred)
-    print("Accuracy: {}".format(acc))
-    return acc
-
-if __name__ == '__main__':
-
-    #leo el dataset
     df = pd.read_csv("../data/imdb_small.csv")
     df['label'] = (df['label'] == 'pos').astype('int')
 
-    MAX_KS_ACC = []
+    TOTAL_TRAIN = 6000
+    text_train, label_train, text_test, label_test = get_instances(df,TOTAL_TRAIN,TEST_SIZE)
 
+    print("Vectorizando...")
+    X_train, y_train, X_test, y_test = vectorizar(text_train, label_train, text_test, label_test, BINARIO, IDF, NEGACIONES, STOP_WORDS)
 
-    for TRAIN_SIZE in range(100, 6000, 400):
-        text_train, label_train, text_test, label_test = get_instances(df,TRAIN_SIZE,500)
-        X_train, y_train, X_test, y_test = vectorizar(text_train, label_train, text_test, label_test)
+    TRAIN = 1000
+    saltoTRAIN = 1000
+    mAcc = []
+    while TRAIN <= 6000:
+        print(TRAIN)
+        clf = KNNClassifier(1)
+        clf.fit(X_train[:TRAIN], y_train[:TRAIN])
+        mat = []
+        mat = clf.testearK(X_test)
+        vAcc = []
+        for i in range(len(mat[0])):
+            a = mat[:, i]
+            acc = accuracy_score(y_test, a)
+            vAcc.append(acc)
 
-        MAX_K_ACC = 0.0
-        for K in range(50, 400, 50):
-            MAX_K_ACC = max(MAX_K_ACC, run_test(df,K = K, TRAIN_SIZE = TRAIN_SIZE))
-        MAX_KS_ACC.append(MAX_K_ACC)
+        mAcc.append(vAcc)
+
+        TRAIN += saltoTRAIN
 
     fout = open("resultados_exp5.pkl","wb")
-    pickle.dump((MAX_KS_ACC),fout)
+    pickle.dump(mAcc,fout)
     fout.close()
